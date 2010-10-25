527809c320e4e21a6ec226723fa75f578f903260
Kevin <kevin.hutchinson@legendum.com>
2010-10-25 00:54:43 -0500

    Better style

--- app/views/en/default/index.php
+++ app/views/en/default/index.php
@@ -1,6 +1,6 @@
 <h3>Online Sangha in the tradition of Thich Nhat Hanh</h3>
 
-<img src="/images/lotus-tea-anyone.jpg" width="500" height="375" align="right" />
+<img id="photo" class="padded-left" src="/images/lotus-tea-anyone.jpg" width="500" height="375" align="right" alt="Inspirational photo" />
 <p>
    Plumline is not a replacement for<br/>
    face-to-face Sangha, but a viable<br/>
@@ -19,5 +19,5 @@
 <p>
     "Without Sangha the practitioner<br/>
     will quickly lose their practice"<br/>
-    - Thich Nhat Hanh
+    - <a href="http://www.google.com/#hl=en&q=Thich+Nhat+Hanh&fp=1">Thich Nhat Hanh</a>
 </p>
