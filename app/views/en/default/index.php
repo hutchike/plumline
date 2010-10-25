95d877a9392fa168b7b5fd7b5a9152315c694cae
Kevin <kevin.hutchinson@legendum.com>
2010-10-25 00:10:05 -0500

    Home page has content

--- app/views/en/default/index.php
+++ app/views/en/default/index.php
@@ -1,1 +1,18 @@
-Plumline - returning soon
+<h3>Online Sangha In the tradition of Thich Nhat Hanh</h3>
+
+<img src="/images/lotus-tea-anyone.jpg" width="500" height="375" align="right" />
+<p>
+   Plumline is not a replacement for<br/>
+   face-to-face Sangha, but a viable<br/>
+   alternative for those who cannot <br/>
+   regularly attend traditional Sangha.
+</p>
+
+<p>
+   The Plumline website is a support<br/>
+   center for those wishing to<br/>
+   establish, build and maintain an<br/> 
+   online Sangha. A place to share<br/>
+   ideas and resources as online<br/>
+   Sangha develops.
+</p>
