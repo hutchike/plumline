d99638d808185008bf6fac056e873d340702ed2f
Kevin <kevin.hutchinson@legendum.com>
2010-10-25 00:25:25 -0500

    Added links

--- app/views/en/layouts/simple.html.php
+++ app/views/en/layouts/simple.html.php
@@ -1,6 +1,13 @@
 <div id="page">
     <div id="header">
         <a href="/"><img src="/images/plumline.png" width="300" height="109" alt="Plumline" /></a>
+        <div id="links">
+            <ul>
+                <li class="first"><a href="/practice/mindfulness">Mindfulness practice</a></li>
+                <li><a href="/practice/links">Useful links</a></li>
+                <li><a href="/contact/form">Contact form</a></li>
+            </ul>
+        </div>
     </div>
     <div id="content">
 <?= indent($content, 8) ?>
