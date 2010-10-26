ec5a4f90f138e3628db8ff41f0fba225f79f2cbe
Kevin <kevin.hutchinson@legendum.com>
2010-10-26 15:44:28 -0500

    Contact form now works

--- app/controllers/Contact.php
+++ app/controllers/Contact.php
@@ -6,6 +6,18 @@ class Contact_controller extends App_controller
     public function form()
     {
         $this->render->title = 'Contact form';
-        $this->render->member = new Member();
+        $member = new Member($this->params->member);
+        if ($found = $member->find_first(array('email' => $member->email)))
+        {
+            $this->flash->now = 'We\'re sorry but that member has already registered. Please use an alternative email address.';
+        }
+        else
+        {
+            if ($member->save())
+            {
+                $this->redirect('', array('notice' => 'Thank you for your interest. Please expect a friendly email from us in the next few days.'));
+            }
+        }
+        $this->render->member = $member;
     }
 }
