7362d3f38e377caf1d3361f3c5b73c3c962f444c
Kevin <kevin.hutchinson@legendum.com>
2010-10-26 23:40:22 -0500

    Added some links, and fixed page titles

--- app/views/en/practice/links.php
+++ app/views/en/practice/links.php
@@ -1,1 +1,5 @@
-Links go here
+<ul>
+    <li><a href="http://www.plumvillage.org/">Plum Village</a></li>
+    <li><a href="http://www.orderofinterbeing.org/">The Order of Interbeing</a></li>
+    <li><a href="http://www.mindfulnessbell.org/">The Mindfulness Bell magazine</a></li>
+</ul>
