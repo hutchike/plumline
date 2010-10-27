9324a1ed81a5bbd18c318861afe246015267cb3a
Kevin <kevin.hutchinson@legendum.com>
2010-10-26 20:18:49 -0500

    Added form validation

--- app/models/Member.php
+++ app/models/Member.php
@@ -4,8 +4,11 @@ class Member extends Model
     public function setup()
     {
         $this->set_timestamp('created_at', 'updated_at');
+        $this->validates('name', 'is_valid_length', array('shortest' => 3, 'longest' => 70));
         $this->validates('email', 'is_valid_email');
         $this->validates('email', 'is_valid_length', array('shortest' => 6, 'longest' => 70));
+        $this->validates('country', 'is_valid_length', array('shortest' => 2, 'longest' => 70));
+        $this->validates('timezone', 'is_valid_length', array('shortest' => 3, 'longest' => 70));
     }
 }
 
