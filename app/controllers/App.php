c9c30f81a5a12f7409f37fb1e72a3f770125b8be
Kevin <kevin.hutchinson@legendum.com>
2010-10-26 20:45:01 -0500

    Added a "lowsrc" feature

--- app/controllers/App.php
+++ app/controllers/App.php
@@ -9,7 +9,7 @@ class App_controller extends Controller
         $this->render->session_user_email = $this->session->user_email;
         $this->render->user_email = $this->cookie->user_email;
         $this->render->title = 'Online Sangha in the tradition of Thich Nhat Hanh';
-        $this->render->onload = '';
+        $this->render->onload = 'plumline.onload()';
         $this->render->layout = 'simple';
         $this->render->image = $this->get_random_image();
     }
--- app/public/scripts/plumline.js
+++ app/public/scripts/plumline.js
@@ -1,1 +1,9 @@
-// Nothing (yet)
+plumline = {
+    onload: function() {
+        var photo = document.getElementById('photo');
+        if (photo) {
+            var src = new String(photo.src);
+            photo.src = src.replace('/lowsrc', '');
+        }
+    }
+}
