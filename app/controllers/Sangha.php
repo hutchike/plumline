71aae08e60469c9ddfebb657cfcee7a51dc53065
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-10-28 12:04:14 -0500

    Added a history page

---
+++ app/controllers/Sangha.php
@@ -0,0 +1,8 @@
+<?
+class Sangha_controller extends App_controller
+{
+    public function history()
+    {
+        $this->render->title = 'The history of Plumline Sangha';
+    }
+}
---
+++ app/views/en/sangha/history.php
@@ -0,0 +1,33 @@
+<p>March, 2006: Taking Thay's words to heart ~ "Without Sangha practitioners quickly lose their practice" ~ Allan LaCroix, Peaceful Commitment of the Heart, of Canada, sent out an email looking for interested parties to help set up an Online Sangha for the physically challenged community. Because of Allan's own difficulties accessing face-to-face Sangha he thought this might be a viable way for him to keep in contact with the greater Sangha and at the same time maintain his practice.</p>
+
+<p>Elaine Sparrow, Chan Lien Tam, True Lotus Heart, Colorado, USA, having similar difficulties, thought this was a great idea, wanted to help, contacted Allan, and Plumline Sangha, was born.</p>
+
+<p>The 2 began meeting weekly and over time set up a workable online meeting format. To start we used <a href="http://messenger.yahoo.com/">Yahoo Messenger</a>. We had hoped to connect via web cam and voice, found out our different systems weren't compatible for the increased technology and stayed with IM ~ the system we still use.</p>
+
+<p>While in Plum Village in June 2006, Elaine met people who lived in remote places with no Sangha nearby. She saw the potential of the use of Online Sangha by people who are geographically limited as well as the physically limited. Elaine posted signs, put out a sign-up sheet during the Sangha Fair, and spoke with people about Plumline. Comments ranged from "a great idea" to "this will be huge over time".
+
+<p>Allan's goal was to ultimately set up a website where people could connect with one another. He and Elaine put their ideas together and started working on Plumline, the web site.
+
+<p>The Plumline Mission Statement pretty much explains what Plumline is all about:</p>
+<ul>
+    <li>Online Sangha is not a replacement for face-to-face Sangha, but a viable alternative for those that cannot regularly attend a traditional Sangha and wish to maintain their practice.</li>
+    <li>The Plumline website is a support center for those wishing to establish, build, and maintain an online Sangha. A place to share ideas and resources as online Sangha develops.</li>
+    <li>Plumline has been established to enhance the Sangha experience for practitioners and enable people to connect with others in similar situations.</li>
+</ul>
+
+<p>"We created a website that is easy to navigate through and simple to use. Starting at the home page you will find buttons and links to guide you through all pages and information."</p>
+
+<p>Over the first months Elaine found herself facilitating Plumline alone, twice a day on Mondays, trying to accommodate people from around the world ~ 10:00 AM and 6:00 PM Mountain Time Zone.  "We have had requests from 24 countries."  By December of 2008 she was not able to continue as meeting facilitator by herself ~ she had received over 175 requests from people and could only serve 20 or 25 due to time zone and language constraints. Currently Plumline has committed people from the US and Canada.  "We can't serve the people from the various time zones requesting to join us." Bill from Canada took over the evening meeting and now facilitates on Thursdays at 8:00 PM Eastern Time Zone.  Lorraine facilitates on Mondays at 9:00 AM Pacific Time Zone.  Elaine operates behind the scenes answering the many emails that come in.</p>
+
+<p>We hope to see Plumline grow, flourish, and "become huge" over time.</p>
+
+<p>We are in need of volunteers to make this happen:</p>
+<ul>
+    <li>Background technical support for the website and the Plumline message board
+    <li>People to facilitate meetings in other countries to accommodate time zones</li>
+</ul>
+
+<p>Clearly we don't need someone from every country, not yet.  We need folks from time zones that can accommodate Europe, Africa, Asia, and South America.  These are some of the places we have heard from so far ~
+Australia, Austria, Canada, South America, Africa, France, Germany, Greece, India, Ireland, Israel, Malaysia, Mexico, Norway, Scotland, Spain, Syria, Senegal, Thailand, UK, United Arab Emirates, US.</p>
+
+<p>"Plumline has already become more than Allen and I imagined.  It has become a vital worldwide Sangha connection" ~ one we hope will continue for years to come.</p>
