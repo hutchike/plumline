f2ee7064a8d811f3c74ff28f2dfa991e76bf5fde
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-10-28 11:46:30 -0500

    Added the Hokey Pokey

--- app/views/en/practice/links.php
+++ app/views/en/practice/links.php
@@ -2,6 +2,7 @@
     <li><a href="http://www.bluecliffmonastery.org/">Blue Cliff Monastery</a></li>
     <li><a href="http://www.compassionatedharmacloudmonastery.org/">Compassionate Dharma Cloud Monastery</a></li>
     <li><a href="http://www.deerparkmonastery.org/">Deer Park Monastery</a></li>
+    <li><a href="http://kids.niehs.nih.gov/lyrics/hokey.htm">Doing the Hokey Pokey</a> (popular in Plumline Sangha meetings on Monday)</li>
     <li><a href="http://www.plumvillage.org/international-sangha-directory.html">International Sangha Directory</a></li>
     <li><a href="http://www.magnoliavillagesangha.org/">Magnolia Village Mindfulness Center</a></li>
     <li><a href="http://www.mindfulnessbell.org/">The Mindfulness Bell magazine</a></li>
@@ -10,4 +11,4 @@
     <li><a href="http://www.plumvillage.org/">Plum Village</a></li>
 </ul>
 
-<p>If you have a suggestion for a link, please <a href="/contact/form">contact us</a>.
+<p>If you'd like to suggest a link, please <a href="/contact/form">contact us</a>.
