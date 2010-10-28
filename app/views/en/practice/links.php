0d627978d516e1c6768a9e023057a0b5e2e9da64
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-10-28 11:20:12 -0500

    Added new links

--- app/views/en/practice/links.php
+++ app/views/en/practice/links.php
@@ -1,5 +1,13 @@
 <ul>
-    <li><a href="http://www.plumvillage.org/">Plum Village</a></li>
-    <li><a href="http://www.orderofinterbeing.org/">The Order of Interbeing</a></li>
+    <li><a href="http://www.bluecliffmonastery.org/">Blue Cliff Monastery</a></li>
+    <li><a href="http://www.compassionatedharmacloudmonastery.org/">Compassionate Dharma Cloud Monastery</a></li>
+    <li><a href="http://www.deerparkmonastery.org/">Deer Park Monastery</a></li>
+    <li><a href="http://www.plumvillage.org/international-sangha-directory.html">International Sangha Directory</a></li>
+    <li><a href="http://www.magnoliavillagesangha.org/">Magnolia Village Mindfulness Center</a></li>
     <li><a href="http://www.mindfulnessbell.org/">The Mindfulness Bell magazine</a></li>
+    <li><a href="http://www.orderofinterbeing.org/">The Order of Interbeing</a></li>
+    <li><a href="http://www.parallax.org">Parallax Press</a></li>
+    <li><a href="http://www.plumvillage.org/">Plum Village</a></li>
 </ul>
+
+<p>If you have a suggestion for a link, please <a href="/contact/form">contact us</a>.
