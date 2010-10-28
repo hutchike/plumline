fa05eba869e0be2d400658d96ffc943bb0b3cbf7
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-10-28 11:37:06 -0500

    Fixed formatting

--- app/views/en/practice/mindfulness.php
+++ app/views/en/practice/mindfulness.php
@@ -4,4 +4,5 @@
 <ul>
     <li><a href="the-5-mindfulness-trainings">The 5 mindfulness trainings</a> for members of the Sangha</li>
     <li><a href="the-14-mindfulness-trainings">The 14 mindfulness trainings</a> for members of the <a href="http://www.orderofinterbeing.org/">Order of Interbeing</a></li>
+    <li><a href="the-5-hindrances">The 5 hindrances</a> - about obstacles to practice</li>
 </ul>
