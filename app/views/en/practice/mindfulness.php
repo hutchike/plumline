552b951c6cf2c5129d3f139b594c4ae57c6ff2e9
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-11-16 23:49:39 -0600

    Added sangha format info

--- app/views/en/practice/mindfulness.php
+++ app/views/en/practice/mindfulness.php
@@ -6,3 +6,19 @@
     <li><a href="the-14-mindfulness-trainings">The 14 mindfulness trainings</a> for members of the <a href="http://www.orderofinterbeing.org/">Order of Interbeing</a></li>
     <li><a href="the-5-hindrances">The 5 hindrances</a> - about obstacles to practice</li>
 </ul>
+
+<h3>About our Sangha meeting format</h3>
+We follow the following Sangha format:
+<ol>
+    <li>20-25 minute seated meditation (feel free to do walking meditation if you  prefer)<br/>(Note: If your computer can receive "streaming audio and/or video", you are often invited to share in a guided meditation).</li>
+    <li>Reading of the 5 Mindfulness Trainings</li>
+    <li>Discussion for the rest of the hour to 2 hours for those who wish to stay with the discussion.<br/>(We also, sometimes, contemplate a reading by Thay during part of this period).</li>
+</ol>
+
+<p>Plumline members came up with the following guideline which makes online discussion efficient:</p>
+
+<p>Just as in face to face Sangha, we use the bow as a signal that we would like to speak. When you wish to speak, type in the word "bow" and post it so the group knows you wish to speak. Then type your comments and post them, taking as much time as you need. When you are finished speaking post the word "bow" again to let everyone know when you are finished.</p>
+
+<p>This allows each of us to speak without interruption. This also allows us to follow our breath and practice being mindful, fully present, as we wait for the person typing to post their message.</p>
+
+<p>This e-version of a sangha is a different way of communication, one many of us are not used to. Yet we have found it to be a very viable way to connect with like-minded people looking for Sangha connection.</p>
