67dba7a6abfaeeb8ba2fed14edf079c238d9038e
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-04 08:16:22 +0000

    added a second email field

--- app/data/plumline.sql
+++ app/data/plumline.sql
@@ -4,6 +4,7 @@ create table messages
     id          integer unsigned auto_increment primary key,
     name        varchar(255),
     email       varchar(255),
+    email_other varchar(255),
     phone       varchar(255),
     password    varchar(255),
     timezone    varchar(255),
--- app/views/en/contact/mail/message.text.php
+++ app/views/en/contact/mail/message.text.php
@@ -2,7 +2,7 @@ Plumline website message
 ========================
 
 Name: <?= $message->name . "\n" ?>
-Email: <?= $message->email . "\n" ?>
+Email(s): <?= $message->email . ' ' . $message->email_other . "\n" ?>
 Meeting: <?= $message->meeting . "\n" ?>
 Country: <?= $message->country . "\n" ?>
 Timezone: <?= $message->timezone . "\n" ?>
