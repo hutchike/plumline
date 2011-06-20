d826ba1b1490a3237212610e93732a2c4b5caed1
Kevin <kevin.hutchinson@legendum.com>
2011-06-20 17:58:24 -0500

    added something cool

---
+++ app/views/en/practice/the-other.php
@@ -0,0 +1,4 @@
+The Other (from You Are Here by Thich Nhat Hanh)
+
+The first miracle of mindfulness is our true presence - being here, present, and totally alive. Then, if you are really here, something else will also be here: the presence of the other. You are here and the otehr is here. What is the other? It could be your heart; your eyes; your body; or your in-breath. The other is the sunset, the song of the birds. Or it could be your lover, your partner, your son, your daughter, or your friend.
+  The other is the Kingdom of God. If you are really here, solid and free, the Kingdom of God will also be here for you. The fundamental condition for recognizing the presence of the other is your own true presence. What is loving? It is recognizing the presence of the other with your love. This is not a theory; it is a practice.
