08d3339be46a27704fcf731dc4a504858d3068ca
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2012-01-03 12:33:17 -0600

    up-to-date

--- app/views/en/practice/links.php
+++ app/views/en/practice/links.php
@@ -7,7 +7,8 @@
     <li><a href="http://www.magnoliavillagesangha.org/">Magnolia Village Mindfulness Center</a></li>
     <li><a href="http://www.mindfulnessbell.org/">The Mindfulness Bell magazine</a></li>
     <li><a href="http://www.orderofinterbeing.org/">The Order of Interbeing</a></li>
-    <li><a href="http://www.parallax.org">Parallax Press</a></li>
+    <li><a href="http://www.parallax.org/">Parallax Press</a></li>
+    <li><a href="http://www.peacefulpresence.org/">Peaceful Presence Sangha (Online)</a>
     <li><a href="http://www.plumvillage.org/">Plum Village</a></li>
     <li><a href="http://video.google.com/videoplay?docid=-3870733554401994670">Video of Thay answering questions about prayer</a>
 </ul>
