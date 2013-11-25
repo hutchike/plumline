38d0cda33bf479b8738147a5f5ac73432f6ed5de
Kevin Hutchinson <kevin.hutchinson@guanoo.com>
2013-11-25 06:14:35 +0100

    grammar corrections

--- app/views/en/default/index.php
+++ app/views/en/default/index.php
@@ -6,19 +6,20 @@
   </p>
 </div>
 <p>
-Plumline supports the establishment, building and maintainence of online Sanghas in the Thich Nhat Hanh tradition.  It coordinates contact between online sanghas and potential members. It also provides a set of links to <a href="/practice/links">organizations and monasteries</a> of the Thich Nhat Hanh tradition to help visitors find information about texts, talks and retreats.
+Plumline supports the establishment, building and maintainence of online sanghas in the Thich Nhat Hanh tradition.  It coordinates contact between online sanghas and potential members. It also provides a set of links to <a href="/practice/links">organizations and monasteries</a> of the Thich Nhat Hanh tradition to help visitors find information about texts, talks and retreats.
 </p>
 <p>
 The online sanghas currently represented by this website are in English.
 </p>
 <p>
-There are two types of online sanghas represented.  One of the first sanghas in Plumline still uses Yahoo's instant text conferencing.  The more recent sanghas use Google+ audio/video conferencing.  Both types of meetings are live meetings held in real-time, just using different technical means to communicate.
+There are two types of online sangha represented.  One of the first sanghas in Plumline still uses Yahoo's instant text conferencing.  The more recent sanghas use Google+ audio/video conferencing.  Both types of sangha are live meetings held in real-time.
 </p>
 <p>
 To learn about the format of our sanghas, please read our <a href="practice/mindfulness">Mindfulness Practice</a> page.
 </p>
+<p>&nbsp;</p>
 <p>
-For information about our Yahoo Messenger meetings, please fill out our <a href="/contact/form">Contact Form</a>.
+For information about our Yahoo Messenger sangha, please fill out our <a href="/contact/form">Contact Form</a>.
 </p>
 <p>
 For information about our audio/video sanghas on Google+, please see our <a href="https://plus.google.com/u/0/+PlumlineOrg/posts">Google+ page</a>.
