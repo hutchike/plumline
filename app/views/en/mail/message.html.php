b8503f8186549c3582581fcb5229f2ca5c7c9716
Kevin Hutchinson <kevin.hutchinson@guanoo.com>
2013-11-25 06:08:24 +0100

    forgot to fix the email

--- app/views/en/mail/message.html.php
+++ app/views/en/mail/message.html.php
@@ -1,9 +1,6 @@
 <html>
 <h3>Plumline website message</h3>
 <div><strong>Name</strong>: <?= $message->name ?></div>
-<div><strong>Email(s)</strong>: <?= $message->email ?> <?= $message->email_other ?></div>
-<div><strong>Meeting</strong>: <?= $message->meeting ?></div>
-<div><strong>Country</strong>: <?= $message->country ?></div>
-<div><strong>Timezone</strong>: <?= $message->timezone ?></div>
+<div><strong>Email</strong>: <?= $message->email ?></div>
 <p><?= nl2br($message->content) ?></p>
 </html>
--- app/views/en/mail/message.text.php
+++ app/views/en/mail/message.text.php
@@ -2,9 +2,6 @@ Plumline website message
 ========================
 
 Name: <?= $message->name . "\n" ?>
-Email(s): <?= $message->email . ' ' . $message->email_other . "\n" ?>
-Meeting: <?= $message->meeting . "\n" ?>
-Country: <?= $message->country . "\n" ?>
-Timezone: <?= $message->timezone . "\n" ?>
+Email: <?= $message->email . "\n" ?>
 
 <?= $message->content . "\n" ?>
