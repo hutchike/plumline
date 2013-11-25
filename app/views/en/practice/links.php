f8bd8a0a5a43918e2a98eb8a81bbebc6bda0251a
Kevin Hutchinson <kevin.hutchinson@guanoo.com>
2013-11-25 06:19:35 +0100

    fix links

--- app/views/en/practice/links.php
+++ app/views/en/practice/links.php
@@ -3,14 +3,13 @@
     <li><a href="http://www.compassionatedharmacloudmonastery.org/">Compassionate Dharma Cloud Monastery</a></li>
     <li><a href="http://www.deerparkmonastery.org/">Deer Park Monastery</a></li>
     <li><a href="http://kids.niehs.nih.gov/lyrics/hokey.htm">Doing the Hokey Pokey</a> (popular in Plumline Sangha meetings on Monday)</li>
-    <li><a href="http://www.plumvillage.org/international-sangha-directory.html">International Sangha Directory</a></li>
-    <li><a href="http://www.magnoliavillagesangha.org/">Magnolia Village Mindfulness Center</a></li>
+    <li><a href="http://plumvillage.org/about/international-sangha-directory/">International Sangha Directory</a></li>
+    <li><a href="http://magnoliagrovemonastery.org/">Magnolia Grove Monastery</a></li>
     <li><a href="http://www.mindfulnessbell.org/">The Mindfulness Bell magazine</a></li>
     <li><a href="http://www.orderofinterbeing.org/">The Order of Interbeing</a></li>
     <li><a href="http://www.parallax.org/">Parallax Press</a></li>
     <li><a href="http://www.peacefulpresence.org/">Peaceful Presence Sangha (Online)</a>
     <li><a href="http://www.plumvillage.org/">Plum Village</a></li>
-    <li><a href="http://video.google.com/videoplay?docid=-3870733554401994670">Video of Thay answering questions about prayer</a>
 </ul>
 
 <p>To suggest a link, please <a href="/contact/form">contact us</a>.
